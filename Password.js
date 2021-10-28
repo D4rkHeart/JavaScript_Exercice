a5be003cefce78da6f0fecb73581845020f83a69
D4rkHeart <leonardo.surdez@gmail.com>
2021-10-28 13:47:06 +0200

    [Update] Added a minor syntax modification

--- Password.js
+++ Password.js
@@ -1,7 +1,7 @@
 const REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}$/
 const readline = require('readline').createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   })
   //Ask input form user
   readline.question(`Write your password : `, userInput => {
@@ -13,13 +13,11 @@ const readline = require('readline').createInterface({
       case true:
         console.log("Congratulation you know how to create a strong password")
         break;
-    
       case false :
         console.log("Error : your password need to include one capital letter, one number,and one number or symbol and be at least 8 caracter long")
         break;
     }
   })
-
 /*FastSolution : 
 Var readlineSync = require('readline-sync');
 FastSolution
@@ -27,3 +25,4 @@ password = readlineSync.questionNewPassword();
 console.log('-- Password is ' + password);                      
 console.log('Login ...');
 */                                     
+
