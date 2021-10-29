c3dea34380e5ff21efe91cc9789148c5fc07ac0c
D4rkHeart <leonardo.surdez@gmail.com>
2021-10-29 15:59:37 +0200

    [Update] Improved indentation

--- RepeatString.js
+++ RepeatString.js
@@ -1,10 +1,10 @@
 function repeatStringNumTimes(str, num) {
 var repeatedStr = "";
-    while (num > 0) {
-      repeatedStr += str;
-      num--;
-    }
-    return repeatedStr;
+  while (num > 0) {
+    repeatedStr += str;
+    num--;
+  }
+  return repeatedStr;
 }
 
 console.log(repeatStringNumTimes("Action Conquers Fear, ", 3))
