ecd70d1cadd1d29a7dc024e1fee624f800315f72
D4rkHeart <leonardo.surdez@gmail.com>
2021-10-29 15:44:03 +0200

    [Added] A algorithm who repeat provided string

---
+++ RepeatString.js
@@ -0,0 +1,10 @@
+function repeatStringNumTimes(str, num) {
+var repeatedStr = "";
+    while (num > 0) {
+      repeatedStr += str;
+      num--;
+    }
+    return repeatedStr;
+}
+
+console.log(repeatStringNumTimes("Action Conquers Fear, ", 3))
